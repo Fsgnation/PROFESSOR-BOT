9c6426675f69e2d2863bb1a44726bce9cbefc7cc
Fsgnation <anshsingh44698@gmail.com>
2024-09-21 07:42:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,13 +11,13 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
+API_ID = int(environ['1467987222'])
+API_HASH = environ['a69c47d62f19d2a1467987222ed1fcc9']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
-WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+WEB_SUPPORT = bool(environ.get("WEBHOOK", 'False')) # for web support on/off
+PICS = (environ.get('PICS' ,'')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
